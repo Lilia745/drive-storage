973ea337885dbc5b7440e0ac2ef3f7397b7224a8
Lilia Hasratyan <hasratyanlilia@gmail.com>
2024-07-29 22:27:42 +0400

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App"> </div>
+    <div className="App">
+      <header>
+        <div className="logo-div">
+          <a href="">OnePage</a>
+        </div>
+      </header>
+    </div>
   );
 }
 
