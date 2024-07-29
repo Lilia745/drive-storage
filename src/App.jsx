e135cdb0c6824d2f5fc40531a108736616352bf7
Lilia Hasratyan <hasratyanlilia@gmail.com>
2024-07-29 22:34:37 +0400

    added nav and style

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,31 @@ function App() {
         <div className="logo-div">
           <a href="">OnePage</a>
         </div>
+        <nav>
+        <ul className="menu-list">
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Services</a>
+          </li>
+          <li>
+            <a href="">Portfolio</a>
+          </li>
+          <li>
+            <a href="">Team</a>
+          </li>
+          <li>
+            <a href="">Pricing</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </nav>
       </header>
     </div>
   );
